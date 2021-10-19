01a9b7dca5e2ab457a1984148bdf73e799426589
Jason McGowan <jmcgowan3@dons.usfca.edu>
2021-10-19 14:48:25 -0700

    RequestReader added message body support

--- src/main/java/HTTPServer/RequestReader.java
+++ src/main/java/HTTPServer/RequestReader.java
@@ -13,12 +13,30 @@ public class RequestReader {
     RequestLine requestLine = RequestLineReader.readRequestLine(isr);
     HashMap<String, String> headers = HeaderReader.readHeaderLines(isr);
     String body;
-    if (messageHasBody(headers)) {
-      body = readBody(isr);
+    if (headers.containsKey(CONTENT_LENGTH_FIELD)) {
+      try {
+        int bodySize = Integer.parseInt(headers.get(CONTENT_LENGTH_FIELD));
+        body = readToString(isr, bodySize);
+      } catch (NumberFormatException e) {
+        throw new RequestException("content-length value not an integer",
+            ResponseCode.CLIENT_ERROR_400_BAD_REQUEST);
+      }
+    } else if (headers.containsKey(TRANSFER_ENCODING_FIELD)) {
+      throw new RequestException(
+          "Server only supports payload bodies using content-length header",
+          ResponseCode.SERVER_ERROR_501_NOT_IMPLEMENTED);
     } else {
-      body = "";
+      body = null;
     }
     return new Request(requestLine, headers, body);
+  }
+
+  private static String readToString(InputStreamReader isr, int length) throws IOException {
+    StringBuilder sb = new StringBuilder(length);
+    for (int i = 0; i < length; i++) {
+      sb.append(isr.read());
+    }
+    return sb.toString();
   }
 
   private static String readBody(InputStreamReader isr) {
