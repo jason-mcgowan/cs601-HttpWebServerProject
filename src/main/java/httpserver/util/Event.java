fa4833d690c50443a5a966ff10546b87f88af67e
Jason McGowan <jmcgowan3@dons.usfca.edu>
2021-11-02 19:47:37 -0700

    Removed async operations in Event class

--- src/main/java/httpserver/util/Event.java
+++ src/main/java/httpserver/util/Event.java
@@ -1,8 +1,6 @@
 package httpserver.util;
 
 import java.util.HashSet;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.BiConsumer;
 
@@ -10,8 +8,6 @@ public class Event<A extends EventArg> {
 
   private final HashSet<BiConsumer<Object, A>> subscribers = new HashSet<>();
   private final ReentrantReadWriteLock subLock = new ReentrantReadWriteLock();
-  private final Object poolLock = new Object();
-  private volatile ExecutorService threadPool;
 
   public void subscribe(BiConsumer<Object, A> subscriber) {
     subLock.writeLock().lock();
@@ -47,33 +43,5 @@ public class Event<A extends EventArg> {
     } finally {
       subLock.readLock().unlock();
     }
-  }
-
-  public void invokeAsync(Object owner, A args) {
-    setupAsyncOperation();
-    subLock.readLock().lock();
-    try {
-      subscribers.forEach(sub -> threadPool.execute(() -> sub.accept(owner, args)));
-    } finally {
-      subLock.readLock().unlock();
-    }
-  }
-
-  public void setupAsyncOperation() {
-    if (threadPool == null) {
-      synchronized (poolLock) {
-        if (threadPool == null) {
-          threadPool = Executors.newCachedThreadPool();
-        }
-      }
-    }
-  }
-
-  public synchronized void close() {
-    if (threadPool == null) {
-      return;
-    }
-    threadPool.shutdown();
-    unsubscribeAll();
   }
 }
